883eda5cf25ba1534cf36cf056b4ec2650d5f09e
Adam Szadkowski <adam@szadkowski.info>
2023-12-05 20:16:50 +0100

    Nest part 1

--- src/test/kotlin/pl/allegro/tdd/Day1Test.kt
+++ src/test/kotlin/pl/allegro/tdd/Day1Test.kt
@@ -1,5 +1,6 @@
 package pl.allegro.tdd
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -8,56 +9,60 @@ import strikt.assertions.isEqualTo
 
 class Day1Test {
 
-    @ParameterizedTest
-    @CsvSource(
-        "12,    12",
-        "1a2,   12",
-        "a1a2f, 12",
-        "1a23,  13",
-        "a2f,   22",
-    )
-    fun `calculate single line`(input: String, output: Int) {
-        expectThat(calculate(input)).isEqualTo(output)
-    }
+    @Nested
+    inner class `part 1` {
 
-    @Test
-    fun `calculate two lines`() {
-        val input = """
+        @ParameterizedTest
+        @CsvSource(
+            "12,    12",
+            "1a2,   12",
+            "a1a2f, 12",
+            "1a23,  13",
+            "a2f,   22",
+        )
+        fun `calculate single line`(input: String, output: Int) {
+            expectThat(calculate(input)).isEqualTo(output)
+        }
+
+        @Test
+        fun `calculate two lines`() {
+            val input = """
             12
             23
         """.trimIndent()
 
-        expectThat(calculate(input)).isEqualTo(12 + 23)
-    }
+            expectThat(calculate(input)).isEqualTo(12 + 23)
+        }
 
-    @Test
-    fun `test sample data`() {
-        val input = """
+        @Test
+        fun `test sample data`() {
+            val input = """
             1abc2
             pqr3stu8vwx
             a1b2c3d4e5f
             treb7uchet
         """.trimIndent()
 
-        expectThat(calculate(input))
-            .isEqualTo(12 + 38 + 15 + 77)
-            .isEqualTo(142)
+            expectThat(calculate(input))
+                .isEqualTo(12 + 38 + 15 + 77)
+                .isEqualTo(142)
+        }
+
+        @Test
+        fun `calculate input`() {
+            val input = Day1Test::class.java.classLoader.getResource("day-1/input.txt").readText()
+
+            val output = calculate(input)
+
+            expectThat(output).isEqualTo(55712)
+        }
+
+        private fun calculate(input: String): Int =
+            input
+                .split("\n")
+                .sumOf { line ->
+                    val digits = line.filter { it.isDigit() }.map { it.digitToInt() }
+                    digits.first() * 10 + digits.last()
+                }
     }
-
-    @Test
-    fun `calculate input`() {
-        val input = Day1Test::class.java.classLoader.getResource("day-1/input.txt").readText()
-
-        val output = calculate(input)
-
-        expectThat(output).isEqualTo(55712)
-    }
-
-    private fun calculate(input: String): Int =
-        input
-            .split("\n")
-            .sumOf { line ->
-                val digits = line.filter { it.isDigit() }.map { it.digitToInt() }
-                digits.first() * 10 + digits.last()
-            }
 }
